dd27c57a105edff3bd89dccf1e5aad858ae74444
bjlamarca <bryanlamarca@outlook.com>
2025-04-20 15:06:01 -0400

    Start

---
+++ hestia/hvac/sensors.py
@@ -0,0 +1,23 @@
+def gettemp(id):
+  try:
+    mytemp = ''
+    filename = 'w1_slave'
+    f = open('/sys/bus/w1/devices/' + id + '/' + filename, 'r')
+    line = f.readline() # read 1st line
+    crc = line.rsplit(' ',1)
+    crc = crc[1].replace('\n', '')
+    if crc=='YES':
+      line = f.readline() # read 2nd line
+      mytemp = line.rsplit('t=',1)
+    else:
+      mytemp = 99999
+    f.close()
+ 
+    return int(mytemp[1])
+ 
+  except:
+    return 99999
+ 
+def test_temp():
+    id = '28-048200877df0'
+    print ("Temp : " + '{:.3f}'.format(gettemp(id)/float(1000)))
---
+++ hestia/main.py
@@ -0,0 +1,3 @@
+from hvac.sensors import test_temp
+
+test_temp()
